c2210613d7b8f200c3d55c45bc3508cba16cf498
Rayhan Putra Randi <rayhan.putra11@ui.ac.id>
2024-12-04 16:19:04 +0700

    fix: query prefix

--- query/sparql.py
+++ query/sparql.py
@@ -18,13 +18,14 @@ class SPARQL:
         self.prefix = """
 PREFIX : <http://project.org/data/>
 PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
-PREFIX rdfs: <http://www.w3.org/2000/rdf-schema#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
 PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
 PREFIX v: <http://project.org/vocab#>
 """
 
     def execute(self, query: str) -> Any:
         """Executes given query to GraphDB."""
+        print(query)
         self.sparql.setQuery(query)
         try:
             ret = self.sparql.queryAndConvert()
