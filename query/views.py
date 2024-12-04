9ebf7a69dc975d424844500ae24d6c1c76abaa53
Rayhan Putra Randi <rayhan.putra11@ui.ac.id>
2024-12-04 14:51:07 +0700

    feat: init skeleton views

--- query/views.py
+++ query/views.py
@@ -3,4 +3,19 @@ from django.http import (
 )
 
 
-def 
+def list_characters(request):
+    if request.method == 'GET':
+        pass
+
+def get_character(request):
+    if request.method == 'GET':
+        pass
+
+def list_episodes(request):
+    if request.method == 'GET':
+        pass
+
+def get_episode(request):
+    if request.method == 'GET':
+        pass
+    
